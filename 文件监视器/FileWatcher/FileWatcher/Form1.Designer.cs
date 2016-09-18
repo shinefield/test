b785651450b58ca49cee047256f97cf1a8085b5d
shinefield <1149762015@qq.com>
2016-09-18 14:02:15 +0800

    文件监视器代码增加

---
+++ 文件监视器/FileWatcher/FileWatcher/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FileWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonClearLog = new System.Windows.Forms.Button();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择监视目录：";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(107, 18);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(461, 21);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(574, 16);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "浏 览";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.BackColor = System.Drawing.Color.Black;
+            this.textBoxLog.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBoxLog.Location = new System.Drawing.Point(12, 79);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(637, 350);
+            this.textBoxLog.TabIndex = 3;
+            this.textBoxLog.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxLog_KeyDown);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(233, 50);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "开始监控";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonClearLog
+            // 
+            this.buttonClearLog.Location = new System.Drawing.Point(574, 50);
+            this.buttonClearLog.Name = "buttonClearLog";
+            this.buttonClearLog.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearLog.TabIndex = 6;
+            this.buttonClearLog.Text = "清空记录";
+            this.buttonClearLog.UseVisualStyleBackColor = true;
+            this.buttonClearLog.Click += new System.EventHandler(this.buttonClearLog_Click);
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Location = new System.Drawing.Point(143, 52);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(84, 21);
+            this.textBoxType.TabIndex = 7;
+            this.textBoxType.Text = "*.*";
+            this.textBoxType.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "要监控的文件或类型：";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 432);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(149, 12);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://www.shinefield.cn";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(314, 50);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 10;
+            this.buttonStop.Text = "停止监控";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 458);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxType);
+            this.Controls.Add(this.buttonClearLog);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FileWatcher";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.Button buttonClearLog;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ 文件监视器/FileWatcher/FileWatcher/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileWatcher
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            string content = (e.FullPath + " 被更改");
+            new Thread(new ParameterizedThreadStart(Record)).Start(content);
+        }
+
+        private void fileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            string content = (e.FullPath + " 被创建");
+            new Thread(new ParameterizedThreadStart(Record)).Start(content);
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+            string content = (e.FullPath + " 被删除");
+            new Thread(new ParameterizedThreadStart(Record)).Start(content);
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            string content = (e.OldFullPath + " 重命名为 " + e.FullPath);
+            new Thread(new ParameterizedThreadStart(Record)).Start(content);
+        }
+
+        private void Record(object content)
+        {
+            Log(content.ToString());
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("iexplore.exe", "http://www.shinefield.cn");
+        }
+
+        private void textBoxLog_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                textBoxLog.SelectAll();
+            }
+        }
+
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBoxPath.Text = folderBrowserDialog1.SelectedPath;
+            folderBrowserDialog1.SelectedPath = textBoxPath.Text;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBoxPath.Text))
+            {
+                Log("路径错误，请选择正确路径！");
+                return;
+            }
+            string path = textBoxPath.Text;
+            string type = textBoxType.Text;
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.Path = path;
+            fileSystemWatcher1.IncludeSubdirectories = true;
+            fileSystemWatcher1.Filter = type;
+            fileSystemWatcher1.EndInit();
+            SetControlEnabled(false);
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            fileSystemWatcher1.EnableRaisingEvents = false;
+            SetControlEnabled(true);
+        }
+
+        private void buttonClearLog_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Text = string.Empty;
+        }
+
+
+        private delegate void SetTextBoxTextDelegate(string text, TextBox textBox);
+        private void SetTextBoxText(string text,TextBox textBox)
+        {
+            if (textBox.InvokeRequired)
+            {
+                Invoke(new SetTextBoxTextDelegate(SetTextBoxText), new object[] { text, textBox });
+            }
+            else
+            {
+                textBox.Text = text;
+            }
+        }
+
+        private delegate void SetControlEnabledDelegte(bool b, Control control);
+        private void SetControlEnabled(bool b, Control control)
+        {
+            if (control.InvokeRequired)
+            {
+                Invoke(new SetControlEnabledDelegte(SetControlEnabled), new object[] { b, control });
+            }
+            else
+            {
+                control.Enabled = b;
+            }
+        }
+
+        private void SetControlEnabled(bool b)
+        {
+            SetControlEnabled(b, textBoxPath);
+            SetControlEnabled(b, buttonBrowse);
+            SetControlEnabled(b, textBoxType);
+            SetControlEnabled(b, buttonStart);
+        }
+
+        private void Log(string str)
+        {
+            SetTextBoxText(" 【" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "】" + str + System.Environment.NewLine + textBoxLog.Text, textBoxLog);
+        }
+
+        
+    }
+}
